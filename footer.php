a17851dfabcee46c5701fb635469d86fdc7b6d11
Gswaraw37 <genta.swara@gmail.com>
2024-03-26 14:18:31 +0700

    tugas PHP pertama

---
+++ footer.php
@@ -0,0 +1,18 @@
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <center>
+                    <span class="badge badge-default">Design By: Genta Swara &copy; 2024 Studi Independen PT. Nurul Fikri Cipta Inovasi</span>
+                </center>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
---
+++ index.php
@@ -0,0 +1,11 @@
+<frameset rows="10%, *, 4%" frameborder="no"> <!--awal baris ke 1-->
+    <frame src="header.php" scrolling="no">
+
+    <frameset cols="*"> <!--ini baris ke 2-->
+        <frame src="home.php" scrolling="yes" name="area">
+    </frameset>
+
+    <frameset>
+        <frame src="footer.php">
+    </frameset>
+</frameset>
